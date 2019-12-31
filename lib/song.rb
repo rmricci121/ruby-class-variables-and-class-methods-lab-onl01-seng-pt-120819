69f85695eb18083ab86d8802233b3a3ea48f674f
Robert M Ricci <rmricci121@gmail.com>
2019-12-31 11:41:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  
+  @@genres = []
   
   def initialize(name, artist, genre) 
     @@count += 1 
@@ -16,7 +16,7 @@ class Song
   end
   
   def self.genres
-  
+    
   end
 
 
