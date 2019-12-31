727854b1475d88ec782357c3aca812e9bd01175e
Robert M Ricci <rmricci121@gmail.com>
2019-12-31 11:31:52 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genre = []
+  
+  def intialize 
+    @@count += 1 
+    @@genre << 
+  end
+  
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genre
+  
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+end
