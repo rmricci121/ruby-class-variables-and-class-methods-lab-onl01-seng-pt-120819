4342a21d0cfe9654cf0596a891056cb553b7122c
Robert M Ricci <rmricci121@gmail.com>
2019-12-31 11:59:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,9 @@ class Song
   @@artists = []
   
   def initialize(name, artist, genre) 
-   
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1 
     @@genres << genre 
     @@artists << artist
